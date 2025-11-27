44651d80221f1c65fa3e8f3967f2e9f1dbda2fb0
harshsahu383 <harshsahu1917@gmail.com>
2025-11-27 12:49:51 +0530

    Connection Established : Database

---
+++ Backend/utils/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+module.exports = async function connectDB(uri) {
+  try {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('MongoDB connected successfully');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
+};
