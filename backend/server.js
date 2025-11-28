049bd5e2b8047661a34243c0e8b50e99231889f9
harshsahu383 <harshsahu1917@gmail.com>
2025-11-28 14:48:50 +0530

    Fixing

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,33 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const connectDB = require("./utils/db");
-const contactRoute = require("./routes/contact");
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const connectDB = require('./utils/db');
+const contactRoute = require('./routes/contact');
 
 const app = express();
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://harsh-portfolio.vercel.app"
-  ]
+  origin: "http://localhost:5173",
 }));
-
+app.use(cors());
 app.use(bodyParser.json());
 
-connectDB(process.env.MONGO_URI);
+
+const uri = process.env.MONGO_URI;
+if (!uri) {
+  console.error('MONGO_URI not set in .env');
+} else {
+  connectDB(uri);
+}
 
 
-app.use("/api/contact", contactRoute);
-
+app.use('/api/contact', contactRoute);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
