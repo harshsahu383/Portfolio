8b5a1bc9cea7451cc5732da847ad3ad0627b5f95
harshsahu383 <harshsahu1917@gmail.com>
2025-11-28 20:36:36 +0530

    Backend Updated

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('../models/Contact');
+const Contact = require('../models/contactmodel');
 const sendMail = require('../utils/mailer');
 
 router.post('/', async (req, res) => {
